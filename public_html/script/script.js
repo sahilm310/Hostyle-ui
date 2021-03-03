a08b1a2b1034b0a347dce770f35940d29bcc7616
Sahil Miah <smiah008@gold.ac.uk>
2021-03-04 01:06:42 +0300

    img, styling, animation

--- public_html/script/script.js
+++ public_html/script/script.js
@@ -1,6 +1,9 @@
 // 
 // element.classList.toggle("newClass");
-function toggle(){
-    var np = document.getElementById("nextPage");
+function toggle(id){
+    var np = document.getElementById(id);
     np.classList.toggle("inactive");
 }
+const tl = gsap.timeline({ defaults: { ease: "power2.easeOut" } });
+
+tl.to(".tr-main", { y: "0%", duration: 1, stagger: 0.4 });
