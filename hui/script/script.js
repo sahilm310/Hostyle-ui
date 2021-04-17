c93dcb074f292f3f10f3b42dbea1c437b7cfe705
Sahil Miah <smiah008@gold.ac.uk>
2021-04-18 02:14:19 +0300

    add padding | toggle function

--- hui/script/script.js
+++ hui/script/script.js
@@ -4,6 +4,11 @@ function toggle(id){
     var np = document.getElementById(id);
     np.classList.toggle("inactive");
 }
+
+function toggleClass(elem,className) {
+    elem.classList.toggle(className);
+}
+
 const tl = gsap.timeline({ defaults: { ease: "power2.easeOut" } });
 
 tl.to(".tr-main", { y: "0%", duration: 1, stagger: 0.4 });
